a2d2b5e82152544480bb283b6f0bb2f67ebc0ac6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-02-18 12:48:18 +0000

    Output slack_sns_topic ARN
    This needs to be read by remote sources, such as Elasticsearch.

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "logging_buckets" {
   description = "Buckets created for all account logs related"
   value       = module.logging.buckets
 }
+
+output "slack_sns_topic" {
+  description = "Slack integration sns topic name"
+  value       = module.slack_integration.this_slack_topic_arn
+}
