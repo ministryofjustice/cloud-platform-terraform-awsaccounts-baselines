fa71d4a2974f5805ecce277d22b5c6b6195203c7
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-06-08 15:10:45 +0100

    Attach trail_writer policy to the role (#20)
    * Attach trail_writer policy to the role
    
    * Replace id with arn, id has the temp key exposed

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -373,6 +373,13 @@ resource "aws_iam_policy" "trail_writer" {
   policy      = data.aws_iam_policy_document.trail_policy.json
 }
 
+resource "aws_iam_policy_attachment" "trail_writer" {
+  count       = var.enable_logging ? 1 : 0
+  name        = "CloudTrailPolicyAttachmentforCloudwatchLogs-${var.cloudtrail_name}"
+  roles       = [aws_iam_role.cloudtrail_writer[0].name]
+  policy_arn  = aws_iam_policy.trail_writer[0].arn
+}
+
 # Custom CloudWatch metric for Secrets Manager events from CloudTrail
 resource "aws_cloudwatch_log_metric_filter" "secrets_manager_put_secret_value" {
   count       = var.enable_logging ? 1 : 0
@@ -386,7 +393,7 @@ resource "aws_cloudwatch_log_metric_filter" "secrets_manager_put_secret_value" {
     unit     = "Count"
   dimensions = {
         SecretId = "$.requestParameters.secretId"
-        UserID = "$.userIdentity.principalId"
+        UserArn = "$.userIdentity.arn"
       }
   }
 }
@@ -403,7 +410,7 @@ resource "aws_cloudwatch_log_metric_filter" "secrets_manager_delete_secret" {
     unit     = "Count"
   dimensions = {
         SecretId = "$.requestParameters.secretId"
-        UserID = "$.userIdentity.principalId"
+        UserArn = "$.userIdentity.arn"
       }
   }
 }
